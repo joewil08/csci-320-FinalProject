e2c46e74dcf1b6db2b6d6fcf9a68630605e7585a
Valery Louis <louisvalery2022@gmail.com>
2024-12-07 15:35:22 -0500

    allocate and deallocate method completed

--- src/main/java/filesystem/FileSystem.java
+++ src/main/java/filesystem/FileSystem.java
@@ -9,10 +9,12 @@ public class FileSystem {
     private int iNodeNumber;
     private int fileDescriptor;
     private INode iNodeForFile;
+    private FreeBlockList freeBlockList;
 
     public FileSystem() throws IOException {
         diskDevice = new Disk();
         diskDevice.format();
+        freeBlockList = new FreeBlockList();
     }
 
     /***
@@ -29,15 +31,15 @@ public class FileSystem {
         for (int i = 0; i < Disk.NUM_INODES && !isCreated; i++) {
             tmpINode = diskDevice.readInode(i);
             String name = tmpINode.getFileName();
-            if (name.trim().equals(fileName)){
-                throw new IOException("FileSystem::create: "+fileName+
-                        " already exists");
-            } else if (tmpINode.getFileName() == null) {
+            if (tmpINode.getFileName() == null) {
                 this.iNodeForFile = new INode();
                 this.iNodeForFile.setFileName(fileName);
                 this.iNodeNumber = i;
                 this.fileDescriptor = i;
                 isCreated = true;
+            } else if (name.trim().equals(fileName)){
+                throw new IOException("FileSystem::create: "+fileName+
+                        " already exists");
             }
         }
         if (!isCreated) {
@@ -147,6 +149,10 @@ public class FileSystem {
      */
     public String read(int fileDescriptor) throws IOException {
         // TODO: Replace this line with your code
+
+        /* [valery]
+             reads contents of file from its associated data blocks by traversing the inode file pointers and returns it
+         */
         return null;
     }
 
@@ -158,27 +164,160 @@ public class FileSystem {
 
         // TODO: Replace this line with your code
 
+        /* [valery]
+             split data into 512 bytes chunks and allocate a data block for each chunk
+            update the inode with the correct pointers
+         */
+        allocateBlocksForFile(this.iNodeNumber, data.getBytes().length);
+
     }
 
 
     /**
-     * Add your Javadoc documentation for this method
+     * this method will allocate the required number of blocks from the freeblock list and also update
+     * the pointers of the inode
      */
     private int[] allocateBlocksForFile(int iNodeNumber, int numBytes)
             throws IOException {
 
-        // TODO: replace this line with your code
+        int numOfBlocksNeeded = (int) Math.ceil((double) numBytes / 512);
+        int[] blockPointers = new int[numOfBlocksNeeded];
+        int missing = numOfBlocksNeeded;
+        int index = 0;
+        int maximumSize = freeBlockList.getFreeBlockList().length * 8;
 
-        return null;
+        System.out.println("Need to allocate " + numOfBlocksNeeded + " data blocks for iNodeNumber "+iNodeNumber);
+
+        for(int i = 0; i < maximumSize; i++){
+            if(isBlockFree(i)){
+                blockPointers[index] = i;
+                freeBlockList.allocateBlock(i);
+                index++;
+                missing--;
+            }
+            // found enough free blocks, no need to keep looking
+            if(missing <= 0){
+                break;
+            }
+        }
+
+        printBlocksAllocated(iNodeNumber, blockPointers);
+
+        // setting the pointers to the data blocks of the file
+        for(int i = 0; i < blockPointers.length; i++){
+            iNodeForFile.setBlockPointer(i, blockPointers[i]);
+        }
+
+        // setting the size to the amount of data blocks for the file
+        iNodeForFile.setSize(blockPointers.length);
+
+        // checking if block pointer values have ACTUALLY updated
+        System.out.print("the block pointers allocated and updated for inodeNumber: " + iNodeNumber + " = [");
+        for(int i = 0; i < blockPointers.length; i++){
+            if(i == blockPointers.length-1){
+                System.out.print(iNodeForFile.getBlockPointer(i));
+            }
+            else{
+                System.out.print(iNodeForFile.getBlockPointer(i) + ", ");
+            }
+        }
+        System.out.println("]");
+
+        return blockPointers;
+    }
+
+    public boolean isBlockFree (int blockNumber){
+        int blockNum = blockNumber / 8;
+        int offset = blockNumber % 8;
+        return (freeBlockList.getFreeBlockList()[blockNum] & (1 << offset)) == 0;
+    }
+
+    public void printBlocksAllocated(int iNodeNumber, int[] blockPointers){
+        System.out.print("blocks allocated for iNodeNumber "+iNodeNumber+ " = [");
+        for(int i = 0; i < blockPointers.length; i++){
+            if (i == blockPointers.length-1){
+                System.out.print(blockPointers[i]);
+            }
+            else {
+                System.out.print(blockPointers[i] + ", ");
+            }
+        }
+        System.out.print("]");
+        System.out.println();
+    }
+
+    /*
+    [valery] for testing purposes: to check if allocation is working properly for first 8 blocks of data
+    this method will be deleted before submitting assignment
+     */
+    public void printFirst8Blocks(){
+        for (int i = 0; i < 8; i++){
+            System.out.println((freeBlockList.getFreeBlockList()[0] & (1 << i)) != 0);
+        }
     }
 
     /**
-     * Add your Javadoc documentation for this method
+     * get blocks method will get all the data blocks from the inode to be deleted
+     * using those pointers, deallocate method will call deallocateBlock for each of them
      */
     private void deallocateBlocksForFile(int iNodeNumber) {
-        // TODO: replace this line with your code
+        // You may add any private method after this comment
+        try{
+            int[] blocks = getBlocks(iNodeNumber);
+            System.out.print("blocks: [");
+            for(int i = 0; i < blocks.length; i++){
+                freeBlockList.deallocateBlock(blocks[i]);
+                if(i == blocks.length-1){
+                    System.out.print(blocks[i]);
+                }
+                else{
+                    System.out.print(blocks[i] + ", ");
+                }
+            }
+            System.out.println("] deleted for inode: "+iNodeNumber);
+        } catch (IOException ioException){
+            System.out.println("Unable to deallocate blocks for iNodeNumber " +iNodeNumber);
+        }
     }
 
-    // You may add any private method after this comment
+    /**
+        Searches for all the data blocks associated with an inode and returns a list of those blocks
+     **/
+    private int[] getBlocks(int inodeNumber) throws IOException{
+        INode tmpINode = null;
+        int numOfDataBlocks = -1;
+        for(int i = 0; i < Disk.NUM_INODES; i++){
+            if(i == inodeNumber){
+                tmpINode = diskDevice.readInode(i);
+                numOfDataBlocks = tmpINode.getSize();
+                break;
+            }
+        }
 
+        int[] blocksToDelete = new int [numOfDataBlocks];
+
+        if(numOfDataBlocks != -1){
+            for(int i = 0; i < Disk.NUM_INODES; i++){
+                if(i == inodeNumber){
+                    tmpINode = diskDevice.readInode(i);
+                    // get each data block that will be deleted
+                    for(int j = 0; j < numOfDataBlocks; j++){
+                        blocksToDelete[j] = tmpINode.getBlockPointer(j);
+                    }
+                }
+            }
+        }
+        return blocksToDelete;
+    }
+
+    // <!-- For testing purposes --!>
+    public int getNumberOfBlocksAllocated(){
+        int result = 0;
+        for (int i = 0; i < freeBlockList.getFreeBlockList().length * 8; i++){
+            if(!isBlockFree(i)){
+                result++;
+            }
+        }
+        return result;
+    }
 }
